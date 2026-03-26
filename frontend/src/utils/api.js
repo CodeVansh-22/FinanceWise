1b933b964a913fef75cdcc77a556fcc99efa293a
CodeVansh-22 <vanshchauhand@gmail.com>
2026-03-26 16:28:16 +0530

    Update API base URL structure to include /api suffix

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken, removeToken } from './auth';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: `${process.env.REACT_APP_API_URL}/api`,
 });
 
 api.interceptors.request.use((config) => {
